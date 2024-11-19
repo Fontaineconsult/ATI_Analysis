2ecb4793e599326f2d6f67aa1af6696199dd77c3
Daniel Fontaine <fontaine@sfsu.edu>
2024-11-18 16:42:25 -0800

    testig for deploy

---
+++ deploy_to_dprc_server.cmd
@@ -0,0 +1,1 @@
+robocopy "C:\Users\913678186\IdeaProjects\ATI_Analysis\app" "\\130.212.104.18\www\ati" /E /Z /R:2 /W:5 /XD "C:\Users\913678186\IdeaProjects\ATI_Analysis\app\frontend\src\node_modules"
