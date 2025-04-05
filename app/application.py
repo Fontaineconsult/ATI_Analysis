541c43639d255bed91a6e6f74af7973e84f3f512
Daniel Fontaine <fontaine@sfsu.edu>
2025-04-05 14:04:37 -0700

    python312

--- app/application.py
+++ app/application.py
@@ -1,6 +1,8 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(r"C:\www\ati\app")
+
 from app import create_app
 
 app = create_app()
--- app/wsgi.py
+++ app/wsgi.py
@@ -7,14 +7,15 @@ import os
 import sys
 import os
 import logging
-sys.path.append(r"C:\www\ati")
-sys.path.append(r"C:\www\ati\ati")
-sys.path.insert(0, os.path.dirname(__file__))
-# Configure logging to output to stderr
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+# Add the parent directory (C:\www\ati\app) to the path so `app` can be imported
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 try:
-    from app import create_app  # Adjust the import as necessary
+    sys.path.insert(0, os.path.dirname(__file__))
+    # Configure logging to output to stderr
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+    from . import create_app  # Adjust the import as necessary
     application = create_app()
 except Exception:
     logging.exception("An exception occurred while creating the application.")
